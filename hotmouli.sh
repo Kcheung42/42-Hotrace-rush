66d32f2ff36f33a5f041814ba332176d911eb0a0
Kenneth Cheung <kcheung@student.42.us.org>
2017-05-14 14:26:30 -0700

    hotmouli testing script

---
+++ hotmouli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'test01'
+./hotmouli ./hotrace resources/test01.htr
+echo 'test02'
+./hotmouli ./hotrace resources/test02.htr
+echo 'test03'
+./hotmouli ./hotrace resources/test03.htr
+echo 'test04'
+./hotmouli ./hotrace resources/test04.htr
+echo 'test05'
+./hotmouli ./hotrace resources/test05.htr
+echo 'test06'
+./hotmouli ./hotrace resources\ 2/test06.htr
+echo 'test07'
+./hotmouli ./hotrace resources\ 2/test07.htr
